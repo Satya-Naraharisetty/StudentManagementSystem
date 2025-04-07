5046cad6e0cb76467a8c0b0a9fb12aefb0ff893b
Satya Naraharisetty <venkatnaraharisetty1234@gmail.com>
2025-04-07 20:48:14 +0530

    complete project, can change ui if needed

---
+++ src/main/java/com/sbproj/sms/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.sbproj.sms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sbproj.sms.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
---
+++ src/main/java/com/sbproj/sms/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.sbproj.sms.service;
+
+import java.util.List;
+
+import com.sbproj.sms.entity.Student;
+
+public interface StudentService {
+	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student);
+	
+	Student getStudentById(Long id);
+	
+	Student updateStudent(Student student);
+	
+	void deleteStudentById(Long id);
+}
