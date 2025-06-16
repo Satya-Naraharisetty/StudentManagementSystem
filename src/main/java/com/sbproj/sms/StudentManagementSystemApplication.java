264aa5ef6e1674ed6f28283242beb1ae84ad14aa
Satya Naraharisetty <venkatnaraharisetty1234@gmail.com>
2025-06-16 20:22:40 +0530

    added search feature

--- src/main/java/com/sbproj/sms/StudentManagementSystemApplication.java
+++ src/main/java/com/sbproj/sms/StudentManagementSystemApplication.java
@@ -12,7 +12,6 @@ public class StudentManagementSystemApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
-
 	}
 
 	@Autowired
